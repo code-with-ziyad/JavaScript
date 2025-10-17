2154bdc37a1ca5220972c185c2a727f4b57952da
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-10-17 17:36:31 +0500

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,14 @@
+
+
+let person ={       //   Object  
+    name :"ziyad",
+    age : 20,
+    city : "karachi"
+}
+
+console.log(person.name);
+
+
+console.log(typeof null);    // null ka type object hotha hai
+console.log(typeof undefined); // undefined ka type undefined rehta hai
+
