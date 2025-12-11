71dbe4d0015b120ed3076329ce64abc783c94cae
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-12-11 21:40:05 +0500

    Add files via upload

---
+++ objects2.js
@@ -0,0 +1,38 @@
+// let obj1 = new Object();  // Sinleton
+let userOne = {} // Non Singleton
+
+userOne.id = 324;
+userOne.name = "Ziyad"
+userOne.isLogedIn = false;
+
+// console.log(userOne);
+
+let anOtherUser = {
+      email : "ziyad@gmail.com",
+    userId : 353, 
+      fullName:{
+        firstName: "Ziyad",
+        lastName: "Ahmed",
+       }
+     
+}
+// console.log(anOtherUser.fullName);
+
+
+let obj3 = {1:"a", 2:"b"}
+let obj4 = {3:"c", 4:"d"}
+let obj6 = {5:"e", 6:"f"}
+
+
+let obj7 = Object.assign({},obj3,obj4)
+// console.log(obj5);
+
+let obj5 = console.log({...obj3,...obj4,...obj6}) // preffer this
+
+
+console.log(Object.keys(userOne));  // keys == name, id
+console.log(Object.values(userOne)); // values == ziyad, 123
+console.log(Object.entries(userOne)); // entries == both 
+
+console.log(userOne.hasOwnProperty('name')); // check this key is exist in this object
+
