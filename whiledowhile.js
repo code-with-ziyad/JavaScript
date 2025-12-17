1bf67c9e3f001d8672e0132d31b460a19d5c8b1f
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-12-17 12:06:05 +0500

    While , do while

---
+++ whiledowhile.js
@@ -0,0 +1,22 @@
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i = i + 2;
+}
+
+
+let arr = ["Ziyad", "Ali" , "Ahmed"]
+let j = 0;
+while (j < arr.length) {
+    console.log(`value is ${arr[j]}`);
+    j = j + 1;
+}
+
+////////////////////////// do while ////////////////////////////////////////
+
+//  Loop run at least one time compulsery 
+let x = 11
+do {
+    console.log(x);
+    x++
+} while (x < 10);
